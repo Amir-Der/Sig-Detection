453b0aeba1226cfec5e4ab8009f02eec19ab2929
Amir-Der <derakhshan.amir.m@gmail.com>
2025-05-01 15:26:47 +0330

    Update models.py

--- models.py
+++ models.py
@@ -3,14 +3,13 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    has_model = db.Column(db.Boolean, default=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 class Signature(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(128))
-    type = db.Column(db.String(10))  # 'real' or 'fake'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    filename = db.Column(db.String(200), nullable=False)
+    type = db.Column(db.String(10), nullable=False)  # real یا fake
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
