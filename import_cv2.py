d2a8702255deff9f13db91b1ea164bae66519324
Amir-Der <derakhshan.amir.m@gmail.com>
2025-05-02 13:44:00 +0330

    Update import_cv2.py

--- import_cv2.py
+++ import_cv2.py
@@ -1,16 +1,18 @@
 import cv2
 from skimage.feature import hog
 
-def preprocess_image(image_path):
-    img = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
-    if img is None:
-        raise FileNotFoundError(f"تصویر یافت نشد: {image_path}")
+def preprocess_image(file_stream):
+    file_bytes = np.frombuffer(file_stream.read(), np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (150, 150))
     img = cv2.GaussianBlur(img, (3, 3), 0)
     img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                cv2.THRESH_BINARY, 11, 2)
+                                 cv2.THRESH_BINARY, 11, 2)
     return img
 
 def extract_hog_feature(img):
-    return hog(img, orientations=18, pixels_per_cell=(3, 3),
-               cells_per_block=(2, 2), block_norm="L2-Hys")
+    return hog(img,
+               orientations=18,
+               pixels_per_cell=(3, 3),
+               cells_per_block=(2, 2),
+               block_norm="L2-Hys")
