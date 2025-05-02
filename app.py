3f347b804b82fc3fae5e2902b51b26a36f5ad6a4
Amir-Der <derakhshan.amir.m@gmail.com>
2025-05-02 13:30:36 +0330

    Update app.py

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def train_model():
     return jsonify({"result": "⛔ خطا در آموزش مدل."})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=10000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
